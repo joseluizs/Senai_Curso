4b53501ce8c719393a18da4672dd945b887bc040
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-06-01 15:08:59 -0300

    Projeto LH Pet
    Subindo aplicação

--- Program.cs
+++ Program.cs
@@ -1,49 +1,32 @@
-﻿using System.Data.SqlClient;
+using LHPet.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace ProjetoClientes;
+var builder = WebApplication.CreateBuilder(args);
 
-class Program
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseSqlServer("Server=localhost\\SQLEXPRESS; Initial Catalog=LHPet; User Id=sa;Password=85689398; TrustServerCertificate=True;"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, Luiz!");
-
-        try
-        {
-            //Executar
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(
-                "User Id=sa;Password=85689398;"+
-                "Server=localhost\\SQLEXPRESS;" +
-                "Database=projetoclientes;" +
-                "Trusted_Connection=False;"
-            );
-
-            using (SqlConnection conexao = new SqlConnection(builder.ConnectionString))
-            {
-                string sql = "Select * from clientes";
-                using (SqlCommand comando = new SqlCommand(sql, conexao))
-                {
-                    conexao.Open();
-
-                    using (SqlDataReader leitor = comando.ExecuteReader())
-                    {
-                        while (leitor.Read())
-                        {
-                            System.Console.WriteLine("id: {0}", leitor.GetSqlInt32(0));
-                            System.Console.WriteLine("nome: {0}", leitor.GetSqlString(1));
-                            System.Console.WriteLine("email: {0}",leitor.GetSqlString(2));
-                        }
-                    }
-                }
-
-
-            }
-
-        }
-        catch (Exception e)
-        {
-            //exception
-            System.Console.WriteLine("Erro: " + e.ToString());
-        }
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
